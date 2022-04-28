b523a6a1176d29f92bbe9ae92f337bc1b57ae2d5
Swapna Chanda <swapna.chinna@gmail.com>
2022-04-27 23:44:29 -0400

    Added by Swapna

--- src/main/java/org/example/ProgramingApproachDeclvsIntr.java
+++ src/main/java/org/example/ProgramingApproachDeclvsIntr.java
@@ -39,13 +39,14 @@ public class ProgramingApproachDeclvsIntr{
        List<Person> person=List.of(new ProgramingApproachDeclvsIntr.Person("MALE","SAURAV"),
                 new ProgramingApproachDeclvsIntr.Person("FEMALE","SITA"),
                 new ProgramingApproachDeclvsIntr.Person("FEMALE","RENU"),
+               new ProgramingApproachDeclvsIntr.Person("MALE","LAXMANA"),
                new ProgramingApproachDeclvsIntr.Person("MALE","RAM"));
 
 
        //Imperative
         //loop to take out females from the list of persons
         List<Person> personfemale=new ArrayList<>();
-
+        List<Person> personmale=new ArrayList<>();
         System.out.println("Imperative programing");
         for(Person p:person){ //
             if("FEMALE".equalsIgnoreCase(p.gender)) //
@@ -56,11 +57,22 @@ public class ProgramingApproachDeclvsIntr{
             System.out.println(p);
         }
 
+        for(Person p:person){ //
+            if("MALE".equalsIgnoreCase(p.gender)) //
+                personmale.add(p);
+        }
+
+        // loop to print males stored
+
+        for(Person p:personmale){
+            System.out.println(p);
+        }
+
 
         //Declarative
         System.out.println("Declarative programing");
         person.stream().filter(i-> "FEMALE".equalsIgnoreCase(i.gender)).collect(Collectors.toList()).forEach(System.out::println);
-
+        person.stream().filter(i-> "MALE".equalsIgnoreCase(i.gender)).collect(Collectors.toList()).forEach(System.out::println);
 
     }
 }
