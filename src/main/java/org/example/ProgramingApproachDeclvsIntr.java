8387f2722bdd85d759712f3db45ea86fc1bb4402
Sindhura Durga <sindhuradurga@Ravis-MacBook-Air.local>
2022-04-28 19:34:36 -0400

    added address and age

--- src/main/java/org/example/ProgramingApproachDeclvsIntr.java
+++ src/main/java/org/example/ProgramingApproachDeclvsIntr.java
@@ -1,55 +1,103 @@
 package org.example;
 
-import java.net.StandardSocketOptions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProgramingApproachDeclvsIntr{
+public class ProgramingApproachDeclvsIntr {
 
-    static class Person{
+    static class Address {
+
+        private String address;
+        private String state;
+        private String zipCode;
+
+        public Address(String address, String state, String zipCode) {
+            this.address = address;
+            this.state = state;
+            this.zipCode = zipCode;
+        }
+
+      @Override
+        public String toString() {
+            return "Address{" +
+                    "address='" + address + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zipCode='" + zipCode + '\'' +
+                    '}';
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+    }
+
+
+    static class Person {
 
         private String gender;
         private String name;
+        private int age;
+        private Address address;
 
-        public Person(String gender, String name) {
+        public Person(String gender, String name, int age, Address address) {
             this.gender = gender;
             this.name = name;
+            this.age = age;
+            this.address = address;
         }
 
         @Override
         public String toString() {
-            return "Person{" +
+            String s = "Person{" +
                     "gender='" + gender + '\'' +
                     ", name='" + name + '\'' +
-                    '}';
+                    age +
+                    "address='" + address;
+            String s1 = s;
+            return s1;
         }
     }
-    public static void main(String args[]){
-       List<Person> person=List.of(new ProgramingApproachDeclvsIntr.Person("MALE","SAURAV"),
-                new ProgramingApproachDeclvsIntr.Person("FEMALE","APARNA"),
-                new ProgramingApproachDeclvsIntr.Person("FEMALE","MANJU"),
-               new ProgramingApproachDeclvsIntr.Person("MALE","AMIT"));
+
+    public static void main(String args[]) {
+
+        Address address = new Address("test addr", "NY", "345456");
+        Address address1 = new Address("test addr1", "NY", "345456");
+        Address address2 = new Address("test addr2", "NY", "345456");
+        Address address3 = new Address("test addr3", "NY", "345456");
+
+        List<Person> person = List.of(new ProgramingApproachDeclvsIntr.Person("FEMALE", "APARNA", 28, address),
+                new ProgramingApproachDeclvsIntr.Person("MALE", "SAURAV", 32, address1),
+                new ProgramingApproachDeclvsIntr.Person("FEMALE", "MANJU", 42, address2),
+                new ProgramingApproachDeclvsIntr.Person("MALE", "AMIT", 54, address3));
 
 
-       //Imperative
+        //Imperative
         //loop to take out females from the list of persons
-        List<Person> personfemale=new ArrayList<>();
+        List<Person> personfemale = new ArrayList<>();
 
         System.out.println("Imperative programing");
-        for(Person p:person){ //
-            if("FEMALE".equalsIgnoreCase(p.gender)) //
-            personfemale.add(p);
+        for (Person p : person) { //
+            if ("FEMALE".equalsIgnoreCase(p.gender)) //
+                personfemale.add(p);
         }
         //loop to print females stored
-        for(Person p:personfemale){
+        for (Person p : personfemale) {
             System.out.println(p);
         }
 
 
         //Declarative
         System.out.println("Declarative programing");
-        person.stream().filter(i-> "FEMALE".equalsIgnoreCase(i.gender)).collect(Collectors.toList()).forEach(System.out::println);
+        person.stream().filter(i -> "FEMALE".equalsIgnoreCase(i.gender)).collect(Collectors.toList()).forEach(System.out::println);
 
 
     }
