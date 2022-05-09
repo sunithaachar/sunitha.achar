1384d2b49c519014bb6a003557d97f6440a77790
sunithaam <sunitha.achar@gmail.com>
2022-05-09 16:51:36 -0700

    Merge pull request #2 from sunithaachar/sunitha.achar
    [CACLE-102]-Changed names of User as per my convinience

--- src/main/java/org/example/Newclass.java
+++ src/main/java/org/example/Newclass.java
@@ -14,16 +14,16 @@ public class Newclass {
 
 
         Greater(x,y);
-        System.out.println("Enter the age x");
+        System.out.println("Enter the age a");
         Scanner scn2 =new Scanner(System.in);
         int a = scn2.nextInt();
-        System.out.println("Enter the age y");
+        System.out.println("Enter the age b");
         Scanner scn3=new Scanner(System.in);
         int b = scn3.nextInt();
         System.out.println("Enter the age c");
         Scanner scn4=new Scanner(System.in);
         int c = scn4.nextInt();
-        OldAndYoung(a,b,c);
+        OldAndYoung(a,b,c,0,0);
     }
     private static void Greater(int x,int y){
 
@@ -32,20 +32,41 @@ public class Newclass {
         else
             System.out.println("b: is the greater number");
     }
-    private static void OldAndYoung(int a,int b,int c){
-        if(a>b && a>c)
-            System.out.println("a: older age");
-            else
-                System.out.println("b: is the youngest");
-            if(b>a && b>c)
-                System.out.println("a: is the older age");
-            else
-                System.out.println("b: is the youngest");
-            if(c>a && c>b)
+    private static <Char> void OldAndYoung(int a, int b, int c,int  E, int Y){
+       // if(a>b && a>c)
+        //    System.out.println("a: older age");
+        //    else
+         //       System.out.println("b: is the youngest");
+           // if(b>a && b>c)
+             //   System.out.println("a: is the older age");
+            //else
+              //  System.out.println("b: is the youngest");
+            //if(c>a && c>b)
 
-                System.out.println("c:is the older age");
-            else
-                System.out.println("c: is the youngest");
+              //  System.out.println("c:is the older age");
+            //else
+              //  System.out.println("c: is the youngest");
+
+        if ( a > b){
+            E = a;
+            Y = b;
+        }
+        else {
+            E = b;
+            Y = a;
+        }
+
+        if (c > E) {
+            E = c;
+        }
+            if (c < Y){
+                Y = c;
+        }
+
+        System.out.println("Eldest is: " +E );
+     //   System.out.println(E);
+        System.out.println("Youngest is:" +Y);
+      //  System.out.println(Y);
 
     }
 
