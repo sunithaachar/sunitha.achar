153ec8ad2778d6d69b0a92d89b721ac045ec62dc
sunithaam <sunitha.achar@gmail.com>
2022-05-09 10:13:28 -0700

    Merge pull request #2 from sunithaachar/sunitha.achar
    [CACLE-102]-Changed names of User as per my convinience

--- src/main/java/org/example/Newclass.java
+++ src/main/java/org/example/Newclass.java
@@ -14,6 +14,16 @@ public class Newclass {
 
 
         Greater(a,b);
+        System.out.println("Enter the age c");
+        Scanner scn2 =new Scanner(System.in);
+        int c = scn2.nextInt();
+        System.out.println("Enter the age d");
+        Scanner scn3=new Scanner(System.in);
+        int d = scn3.nextInt();
+        System.out.println("Enter the age e");
+        Scanner scn4=new Scanner(System.in);
+        int e = scn4.nextInt();
+        OldAndYoung(c,d,e);
     }
     private static void Greater(int x,int y){
 
@@ -21,6 +31,22 @@ public class Newclass {
             System.out.println("a: is the greater number");
         else
             System.out.println("b: is the greater number");
+    }
+    private static void OldAndYoung(int k,int g,int f){
+        if(k>g && k>f)
+            System.out.println("k: older age");
+            else
+                System.out.println("K: is the youngest");
+            if(g>k && g>f)
+                System.out.println("g: is the older age");
+            else
+                System.out.println("g: is the youngest");
+            if(f>k && f>g)
+
+                System.out.println("f:is the older age");
+            else
+                System.out.println("f: is the youngest");
+
     }
 
     }
