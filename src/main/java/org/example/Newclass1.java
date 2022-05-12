b0c6b59c0c290cbdc2b9835c18db3ce8e38ccd1e
sunithaam <sunitha.achar@gmail.com>
2022-05-12 16:41:29 -0700

    Merge pull request #2 from sunithaachar/sunitha.achar
    [CACLE-102]-Changed names of User as per my convinience

---
+++ src/main/java/org/example/Newclass1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Newclass1 {
+
+    public static void main(String[] args){
+        int i,sum = 0;
+        for (i=0;i<=10;i++){
+             sum=sum+i;
+            System.out.println("Sum of Natural numbers:"+ sum);
+        }
+    }
+}
